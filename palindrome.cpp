0ed773782003639eae22a209b4cf4682df76844f
U-BSD\418367 <418367@SHS-9YBF6X3.beaverton.k12.or.us>
2024-09-18 10:05:47 -0700

    Palindrome

---
+++ palindrome.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+
+/*Nikhil Mantha
+ *C++ & Data Structures
+ *Palindrome
+ */
+
+int main() {
+  cout << "Enter string: ";
+  char str[80];
+  cin.get(str, 80);
+  cin.get();
+
+  //removes punctuation and whitespace
+  char trimmed[80];
+  int count = 0;
+  for (int i = 0; i < strlen(str); i++){
+    if(isalpha(str[i])) { //checks if char is alphabetic
+      trimmed[count] = str[i]; //adds to new cstring
+      count++;
+    }
+  }
+  trimmed[count] = '\0'; //adds terminating char to end cstring
+
+  //reverses cstring
+  char reversed[80];
+  int i = 0;
+  for(i; i < strlen(trimmed); i++){
+    reversed[i] = trimmed[strlen(trimmed)-(i+1)];
+  }
+  reversed[i] = '\0';
+
+  //compares strings to see if palindrome
+  if (strcmp(trimmed, reversed) == 0) {
+    cout << "\"" << str << "\"" << " is a palindrome." << endl;
+  } else {
+    cout << "\"" << str << "\"" << " is NOT a palindrome." << endl;
+  }
+  
+  return 0;
+}
