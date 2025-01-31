0e2d7b8ee2fc5df13cd725b8868eb92042238bcd
U-BSD\418367 <418367@SHS-9YBF6X3.beaverton.k12.or.us>
2025-01-31 13:39:05 -0800

    Hash Table

---
+++ hash/LinkedList.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include "LinkedList.h"
+
+using namespace std;
+
+Student::Student(string s, int i, float f): name(s), id(i), gpa(f) {}
+
+string Student::getName(){ return name; }
+int Student::getID() { return id; }
+float Student::getGPA(){ return gpa; }
+
+Student* Student::makeStudent() {
+  string s;
+  int i;
+  float f;
+  cout << "Enter name: ";
+  getline(cin, name);
+  cout << "Enter ID: ";
+  cin >> id;
+  cout << "Enter GPA: ";
+  cin >> gpa;
+  Student* st = new Student(s, i, f);
+  return st;
+}
+
+Node::Node(Student* s): student(s), next(NULL) {}
+Node::~Node(){
+  delete student;
+  next = NULL;
+}
+
+Node* Node::getNext(){ return next; }
+void Node::setNext(Node* n) {
+  next = n;
+}
+
+Student* Node::getStudent() { return student; }
+
+LinkedList::LinkedList(Node* n): head(n) {}
+
---
+++ hash/LinkedList.h
@@ -0,0 +1,35 @@
+#include <string>
+
+using namespace std;
+
+class Student{
+ private:
+  string name;
+  int id;
+  float gpa;
+ public:
+  Student(string s, int i, float f);
+  string getName();
+  int getID();
+  float getGPA();
+  Student* makeStudent();
+};
+
+class Node{
+ private:
+  Student* student;
+  Node* next;
+ public:
+  Node(Student* s);
+  ~Node();
+  Node* getNext();
+  void setNext(Node* n);
+  Student* getStudent();
+};
+
+class LinkedList{
+ private:
+  Node* head;
+ public:
+  LinkedList(Node* n);
+};
