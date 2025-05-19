82055be4030f1201ec92b47940cdf254a6a6d507
U-BSD\418367 <418367@SHS-C5W2NW3.beaverton.k12.or.us>
2025-05-19 13:24:43 -0700

    BST

---
+++ BST/Node.hpp
@@ -0,0 +1,20 @@
+class Node {
+private:
+  int val;
+  Node* left;
+  Node* right;
+public:
+  Node();
+  int getVal();
+  Node* getLeft();
+  Node* getRight();
+  void setLeft(Node* n);
+  void setRight(Node* n);
+};
+
+class BinaryTree {
+private:
+  Node* root;
+public:y
+  BinaryTree();
+};
